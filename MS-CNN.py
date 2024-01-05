d179f4e182bac9c38c55e0548013a2c45c2f76e9
Ahyeong Jeong <113572871+jxxng00@users.noreply.github.com>
2024-01-05 20:25:11 +0900

    Update MS-CNN.py

--- MS-CNN.py
+++ MS-CNN.py
@@ -24,10 +24,13 @@ def MSCBBlock(block_input):
     return MSCB_out
 
 
-def build_model_3():
+def build_model(model = 'MSCNN'):
+  """
+  model='MSCNN' or 'MSCNN+DE' or 'MSCNN+NPS' or 'MSCNN+all'
+
+  """
 
     initial_lr = 0.1
-
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_lr,
         decay_steps=100,
@@ -39,30 +42,42 @@ def build_model_3():
     concatenated_outputs = []
 
     input_tensor = Input(shape=(3, 1000, 4))
-    DE_input_tensor = Input(shape=(3, 8, 4))
-    NPS_input_tensor = Input(shape=(3, 6, 4))
+    if model in ['MSCNN+DE', 'MSCNN+all']:
+      DE_input_tensor = Input(shape=(3, 8, 4))
+    if model in ['MSCNN+NPS', 'MSCNN+all']:
+      NPS_input_tensor = Input(shape=(3, 6, 4))
 
     for i in range(4):
         input = input_tensor[:, :, :, i]
         block_input = tf.expand_dims(input, axis=-1)
+
         band_output = MSCBBlock(block_input)
         band_output = Conv2D(filters=112, kernel_size=(1, 3), strides=(1, 1), padding='same', activation='relu')(band_output)
         band_output = MaxPooling2D(pool_size=(1, 5), strides=(1, 5), padding='same')(band_output)
 
         for j in range(3):
-            DE_input = DE_input_tensor[:, j, :, i]
-            DE_input_expanded = tf.expand_dims(DE_input, axis=-1)
-            Conv_DE = Conv1D(filters=112, kernel_size=1, activation='relu')(DE_input_expanded)
-            NPS_input = NPS_input_tensor[:, j, :, i]
-            NPS_input_expanded = tf.expand_dims(NPS_input, axis=-1)
-            Con_NPS = Conv1D(filters=112, kernel_size=1, activation='relu')(NPS_input_expanded)
+          if model in ['MSCNN+DE', 'MSCNN+all']:
+              DE_input = DE_input_tensor[:, j, :, i]
+              DE_input_expanded = tf.expand_dims(DE_input, axis=-1)
+              Conv_DE = Conv1D(filters=112, kernel_size=1, activation='relu')(DE_input_expanded)
+          if model in ['MSCNN+NPS', 'MSCNN+all']:
+              NPS_input = NPS_input_tensor[:, j, :, i]
+              NPS_input_expanded = tf.expand_dims(NPS_input, axis=-1)
+              Con_NPS = Conv1D(filters=112, kernel_size=1, activation='relu')(NPS_input_expanded)
 
             band_output_j = band_output[:, j, :, :]
             band_flattend = Flatten()(band_output_j)
-            DE_flattened = Flatten()(Conv_DE)
-            NPS_flattened = Flatten()(Con_NPS)
+            if model == 'MSCNN+DE':
+              DE_flattened = Flatten()(Conv_DE)
+              concat = Concatenate()([band_flattend , DE_flattened])
+            if model == 'MSCNN+NPS':
+              NPS_flattened = Flatten()(Con_NPS)
+              concat = Concatenate()([band_flattend, NPS_flattened])
+            if model == 'MSCNN+all':
+              DE_flattened = Flatten()(Conv_DE)
+              NPS_flattened = Flatten()(Con_NPS)
+              concat = Concatenate()([band_flattend , DE_flattened, NPS_flattened])
 
-            concat = Concatenate()([band_flattend , DE_flattened, NPS_flattened])
             concatenated_outputs.append(concat)
 
     concatenated_output = Concatenate()(concatenated_outputs)
@@ -76,8 +91,13 @@ def build_model_3():
     output_layer = Dense(2, activation='softmax')(output_layer)
 
     # Create the final model
-    model_3 = Model(inputs=[input_tensor, DE_input_tensor, NPS_input_tensor], outputs=output_layer)
+    if model == 'MSCNN+DE':
+      model = Model(inputs=[input_tensor], outputs=output_layer)
+    if model == 'MSCNN+NPS':
+      model = Model(inputs=[input_tensor, DE_input_tensor], outputs=output_layer)
+    if model == 'MSCNN+all':
+      model = Model(inputs=[input_tensor, DE_input_tensor, NPS_input_tensor], outputs=output_layer)
 
     optimizer = SGD(learning_rate=lr_schedule)
-    model_3.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    return model_3
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
